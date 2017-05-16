7592746a5d6836fb4f9853dfa9db2a3d8e455c3e
Admin <admin@Admins-Mac.local>
2017-05-16 17:24:39 +0200

    Initial Commit

---
+++ Sayarti/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sayarti
+//
+//  Created by Admin on 5/16/17.
+//  Copyright © 2017 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
